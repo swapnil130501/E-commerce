e7b6d7b4b047d2f34b8073f37f7db31400df0f61
swapnil <swapnil130501@gmail.com>
2023-09-13 15:30:36 +0530

    created order model

---
+++ src/models/order.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const orderSchema = mongoose.Schema({
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    deliveryType: {
+        type: String,
+        enum: ['1-Day', '2-Day', 'Normal'],
+        required: true,
+        default: 'Normal'
+    },
+    status: {
+        type: String,
+        enum: ['Placed', 'Pending', 'Cancelled', 'Delivered']
+    },
+    transactionId: {
+        type: String
+    }
+}, {timestamps: true});
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
